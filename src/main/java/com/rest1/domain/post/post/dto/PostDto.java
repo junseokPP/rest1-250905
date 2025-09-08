1ef3428fbbe2031f066d31308dbc0936def4c5de
junseokPP <pjs981130@gmail.com>
2025-09-08 10:47:40 +0900

    10강 : record도입

--- src/main/java/com/rest1/domain/post/post/dto/PostDto.java
+++ src/main/java/com/rest1/domain/post/post/dto/PostDto.java
@@ -6,20 +6,21 @@ import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Getter
-@AllArgsConstructor
-public class PostDto {
-    private Long id;
-    private LocalDateTime createDate;
-    private LocalDateTime modifyDate;
-    private String subject;
-    private String body;
-
-    public PostDto(Post post) {
-        this.id = post.getId();
-        this.createDate = post.getCreateDate();
-        this.modifyDate = post.getModifyDate();
-        this.subject = post.getTitle();
-        this.body = post.getContent();
+public record PostDto (
+        Long id,
+        LocalDateTime createDate,
+        LocalDateTime modifyDate,
+        String subject,
+        String body
+) {
+     public PostDto(Post post){
+         this(
+         post.getId(),
+        post.getCreateDate(),
+        post.getModifyDate(),
+        post.getTitle(),
+        post.getContent()
+         );
     }
+
 }
